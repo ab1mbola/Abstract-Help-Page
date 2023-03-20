dce11dbcdc6a91166c5edf6d2c90d8ad191134fb
Abimbola <mustaphaabimbola4@gmail.com>
2023-03-20 17:40:26 +0100

    copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,78 +6,87 @@ export default function Footer() {
     <footer>
       {/* Main Footer */}
       <section className="main">
-        <div className="main-item">
-          <h2 className="title">Abstract</h2>
-          <ul>
-            <li>
-              <a href="#">Start Trial</a>
-            </li>
-            <li>
-              <a href="#">Pricing</a>
-            </li>
-            <li>
-              <a href="#">Download</a>
-            </li>
-          </ul>
-        </div>
+        <div className="container">
+          <div className="main-item">
+            <h2 className="title">Abstract</h2>
+            <ul>
+              <li>
+                <a href="#">Start Trial</a>
+              </li>
+              <li>
+                <a href="#">Pricing</a>
+              </li>
+              <li>
+                <a href="#">Download</a>
+              </li>
+            </ul>
+          </div>
 
-        <div className="main-item">
-          <h2 className="title">Resources</h2>
-          <ul>
-            <li>
-              <a href="#">Blog</a>
-            </li>
-            <li>
-              <a href="#">Help Center</a>
-            </li>
-            <li>
-              <a href="#">Release Notes</a>
-            </li>
-            <li>
-              <a href="#">Status</a>
-            </li>
-          </ul>
-        </div>
+          <div className="main-item">
+            <h2 className="title">Resources</h2>
+            <ul>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Help Center</a>
+              </li>
+              <li>
+                <a href="#">Release Notes</a>
+              </li>
+              <li>
+                <a href="#">Status</a>
+              </li>
+            </ul>
+          </div>
 
-        <div className="main-item">
-          <h2 className="title">Community</h2>
-          <ul>
-            <li>
-              <a href="#">Twitter</a>
-            </li>
-            <li>
-              <a href="#">LinkedIn</a>
-            </li>
-            <li>
-              <a href="#">Facebook</a>
-            </li>
-            <li>
-              <a href="#">Dribbble</a>
-            </li>
-            <li>
-              <a href="#">Podcast</a>
-            </li>
-          </ul>
-        </div>
+          <div className="main-item">
+            <h2 className="title">Community</h2>
+            <ul>
+              <li>
+                <a href="#">Twitter</a>
+              </li>
+              <li>
+                <a href="#">LinkedIn</a>
+              </li>
+              <li>
+                <a href="#">Facebook</a>
+              </li>
+              <li>
+                <a href="#">Dribbble</a>
+              </li>
+              <li>
+                <a href="#">Podcast</a>
+              </li>
+            </ul>
+          </div>
 
-        <div className="main-item">
-          <h2 className="title">Company</h2>
-          <ul>
-            <li>
-              <a href="#">About Us</a>
-            </li>
-            <li>
-              <a href="#">Careers</a>
-            </li>
-            <li>
-              <a href="#">Legal</a>
-            </li>
-            <br />
-            <li>Contact Us</li>
-            <li>
-              <a href="#">info@abstarct.com</a>
-            </li>
-          </ul>
+          <div className="main-item">
+            <h2 className="title">Company</h2>
+            <ul>
+              <li>
+                <a href="#">About Us</a>
+              </li>
+              <li>
+                <a href="#">Careers</a>
+              </li>
+              <li>
+                <a href="#">Legal</a>
+              </li>
+              <br />
+              <li>Contact Us</li>
+              <li>
+                <a href="#">info@abstarct.com</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="copyright">
+            <img src="/assets/abstract-logo.svg" alt="logo" />
+            <p>&copy; Copyright 2023</p>
+            <p>Abstract Studio Design, Inc</p>
+            <p>All rights reserved</p>
+          </div>
         </div>
       </section>
     </footer>
